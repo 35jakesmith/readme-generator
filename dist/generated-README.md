c0d962d1715592450fb886351bdd232933548d07
35jakesmith <35jakesmith@gmail.com>
2022-03-14 20:49:05 -0600

    updated index.js

---
+++ dist/generated-README.md
@@ -0,0 +1,44 @@
+
+    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    
+    # README Generator
+    
+    ## Description
+    
+    Use this to have a readme generated
+    
+    ## Table of Contents
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](questions)
+    
+    ## Installation
+    
+    type the following command in the terminal to install the necessary dependencies: undefined
+    
+    ## Usage
+    
+    once everything is installed, type 'node index.js' in the terminal to start the application.
+    
+    ## License
+    
+    This project is covered under the following license: MIT
+    
+    ## Contributing
+    
+    If you would like to help with this project, please refer to my email in the [questions](#questions) area to get in touch.
+    
+    ## Tests
+    
+    type npm test in the terminal to run the tests (make sure jest has been installed)
+    
+    ## Questions
+    
+    [Github](https://github.com/35jakesmith)
+    
+    If you have any questions, feel free to email me at [35jakesmith@gmail.com](mailto:35jakesmith@gmail.com)
+    
--- index.js
+++ index.js
@@ -1,13 +1,108 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('./src/readme-template');
+const writeFile = require('./utils/generate-page.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub Username? (REQUIRED)',
+            validate: userInput => {
+                if (userInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address? (REQUIRED)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is the name of your project? (REQUIRED)',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please give a name for your project!');
+                    return false;
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please give a description of your project (REQUIRED)' ,
+            validate: descInput => {
+                if (descInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a short description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license for your project. (REQUIRED)',
+            choices: ['Apache', 'MIT', 'Boost', 'Mozilla Public License'],
+            validate: licenseInput => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    console.log('Please select a license');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'dependecies',
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be run to run tests?',
+            default: 'npm test'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What does the user need to know about contribution to the repo?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What does the user need to know about using the repo?'
+        }
+    ]);
+};
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+
+promptUser()
+    .then(readmeData => {
+        return generatePage(readmeData);
+    })
+    .then(readmePage => {
+        return writeFile(readmePage);
+    })
